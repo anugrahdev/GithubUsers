44f76d3716b7a2b72603d4ff44c401e7de79f942
Anang Nugraha <anangnugraha8@gmail.com>
2022-09-16 22:09:29 +0700

    setup VC with tab bar and remove main storyboard

---
+++ GithubUsers/CustomViews/TabBarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarViewController.swift
+//  GithubUsers
+//
+//  Created by Anang Nugraha on 16/09/22.
+//
+
+import Foundation
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavoritesNC()]
+    }
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNC() -> UINavigationController {
+        let favoritesListVC = FavoritesVC()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+}
